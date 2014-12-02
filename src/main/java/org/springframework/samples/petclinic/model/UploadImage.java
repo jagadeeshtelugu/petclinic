4e0b20d2e3c188fe0f434261de10e95a09c0da7f
sambitchoudhury <sambitgudu@yahoo.co.in>
2014-12-02 18:56:41 +0530

    Will act as model attribute when using sevlet 3.0 file upload

---
+++ src/main/java/org/springframework/samples/petclinic/model/UploadImage.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.petclinic.model;
+
+public class UploadImage {
+	public Integer ownerID;
+
+	public Integer getOwnerID() {
+		return ownerID;
+	}
+
+	public void setOwnerID(Integer ownerID) {
+		this.ownerID = ownerID;
+	}
+
+}
